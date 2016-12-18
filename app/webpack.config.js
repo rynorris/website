00ac20e5cb0ed60ffcbcecee4b91655d3a6c1e13
Ryan Norris <rynorris@gmail.com>
2016-12-18 19:27:40 +1100

    Webpack handles compiling typescript

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -5,19 +5,30 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   context: __dirname,
+  
+  devtool: "source-map",
+
   entry: {
     app: [
-      "./build/tmp/app.js",
+      "./src/app.tsx"
     ]
   },
+
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+    ],
+    preLoaders: [
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+
   output: {
     path: path.join(__dirname, "build", "src"),
     filename: "app.js"
   },
-  module: {
-    loaders: [
-    ]
-  },
+
   plugins: [
       new HtmlWebpackPlugin({
 	  inject: false,
@@ -27,5 +38,10 @@ module.exports = {
 	  template: path.resolve(__dirname, "src/index.html"),
 	  title: "Application",
       }),
-  ]
+  ],
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  }
 };
