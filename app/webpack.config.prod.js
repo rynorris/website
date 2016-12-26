dfec3078e17c793a2b31d914fc4ff81767fb5b0b
Ryan Norris <rynorris@gmail.com>
2016-12-26 11:54:29 +0000

    Add prod webpack config and allow index to load hashed app js

---
+++ app/webpack.config.prod.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const path = require("path");
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const baseConfig = require("./webpack.config");
+
+module.exports = Object.assign({}, baseConfig, {
+  devtool: null,
+
+  output: {
+    path: path.join(__dirname, "build", "min", "assets"),
+    filename: "app.[hash].js",
+  },
+
+  plugins: [
+    ...baseConfig.plugins,
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+  ]
+});
