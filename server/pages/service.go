69ecb127233a2314204cdecf3db5868a23d41cbe
Ryan Norris <rynorris@gmail.com>
2016-12-26 17:22:33 +0000

    Add storage backed pages service

---
+++ server/pages/service.go
@@ -0,0 +1,21 @@
+package pages
+
+type CardType string
+
+type Card struct {
+	Type      CardType `json:"type"`
+	Title     string   `json:"title"`
+	Text      string   `json:"text"`
+	ImageLink string   `json:"image"`
+}
+
+type Page struct {
+	Title string `json:"title"`
+	Cards []Card `json:"cards"`
+}
+
+type Service interface {
+	Get(key string) (Page, error)
+	Put(key string, page Page) error
+	Delete(key string) error
+}
---
+++ server/pages/storage/service.go
@@ -0,0 +1,43 @@
+package storage
+
+import (
+	"encoding/json"
+	"github.com/discoviking/website/server/pages"
+	"github.com/discoviking/website/server/storage"
+)
+
+type Service struct {
+	storage storage.Service
+}
+
+func NewService(storage storage.Service) *Service {
+	return &Service{
+		storage: storage,
+	}
+}
+
+func (s *Service) Get(key string) (pages.Page, error) {
+	page := pages.Page{}
+
+	blob, err := s.storage.Get(key)
+	if err != nil {
+		return page, err
+	}
+
+	err = json.Unmarshal(blob, &page)
+
+	return page, err
+}
+
+func (s *Service) Put(key string, page pages.Page) error {
+	blob, err := json.Marshal(page)
+	if err != nil {
+		return err
+	}
+
+	return s.storage.Put(key, blob)
+}
+
+func (s *Service) Delete(key string) error {
+	return s.storage.Delete(key)
+}
