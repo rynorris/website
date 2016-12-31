7d767dd12268abc185386e8fb252f90e59b1daae
Ryan Norris <rynorris@gmail.com>
2016-12-31 12:25:34 +0000

    Add support for responsively sized images on posts

--- app/src/components/post.tsx
+++ app/src/components/post.tsx
@@ -1,16 +1,29 @@
 import * as React from "react";
-import {Card, CardTitle, CardText} from "material-ui/Card";
+import {Card, CardMedia, CardTitle, CardText} from "material-ui/Card";
 
 interface IPostProps {
   title: string;
+  image: string;
 }
 
 export default class Post extends React.Component<IPostProps, {}> {
   render() {
+    let header: JSX.Element;
+    if (this.props.image) {
+      header = (
+        <CardMedia overlay={<CardTitle title={this.props.title} />} >
+          <div className="post-header-image-container">
+            <img className="post-header-image" src={this.props.image}/>
+          </div>
+        </CardMedia>
+      );
+    } else {
+          header = <CardTitle title={this.props.title} />;
+    }
     return (
       <div className="app-post">
         <Card>
-          <CardTitle title={this.props.title} />
+          {header}
           <CardText>{this.props.children}</CardText>
         </Card>
       </div>
