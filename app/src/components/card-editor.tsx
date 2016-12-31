5f00efc7ff2a09450fa8e33ebfda107a1efaadcd
Ryan Norris <rynorris@gmail.com>
2016-12-31 18:24:40 +0000

    Add image selector to card editor

--- app/src/components/card-editor.tsx
+++ app/src/components/card-editor.tsx
@@ -4,6 +4,7 @@ import FlatButton from "material-ui/FlatButton";
 import TextField from "material-ui/TextField";
 import {Card} from "../services/pages-service";
 import DynamicCard from "./dynamic-card";
+import ImageSelector from "./image-selector";
 
 interface ICardEditorProps {
   onSave: (card: Card) => void;
@@ -14,25 +15,26 @@ interface ICardEditorProps {
 
 interface ICardEditorState {
   card?: Card;
+  imageSelectorOpen: boolean;
 }
 
 export default class CardEditor extends React.Component<ICardEditorProps, ICardEditorState> {
   constructor(props: ICardEditorProps) {
     super(props);
+    this.state = {
+      imageSelectorOpen: false,
+    };
+
     if (props.card) {
       let clonedCard: Card = JSON.parse(JSON.stringify(props.card));
-      this.state = {card: clonedCard};
-    } else {
-      this.state = {};
+      this.state.card = clonedCard;
     }
   }
 
   componentWillReceiveProps(props: ICardEditorProps) {
     if (props.card) {
       let clonedCard: Card = JSON.parse(JSON.stringify(props.card));
-      this.state = {card: clonedCard};
-    } else {
-      this.state = {};
+      this.setState(Object.assign({}, this.state, { card: clonedCard }));
     }
   }
 
@@ -77,25 +79,48 @@ export default class CardEditor extends React.Component<ICardEditorProps, ICardE
             multiLine={true}
             rows={4} />
         </div>
+        <FlatButton label="Choose Image" onTouchTap={this.openImageSelector.bind(this)} />
         <h3>Preview</h3>
         <DynamicCard card={this.state.card} />
+        <ImageSelector
+          open={this.state.imageSelectorOpen}
+          onRequestClose={this.closeImageSelector.bind(this)}
+          onDone={this.setImage.bind(this)}
+          />
       </Dialog>
     );
   }
 
   private setTitle(ev: any) {
-    let newState: ICardEditorState = this.state;
-    if (newState.card) {
-      newState.card.title = ev.currentTarget.value;
-      this.setState(newState);
+    let card: Card | undefined = this.state.card;
+    if (card) {
+      card.title = ev.currentTarget.value;
+      this.setState(Object.assign({}, this.state, { card: card }));
     }
   }
 
   private setText(ev: any) {
-    let newState: ICardEditorState = this.state;
-    if (newState.card) {
-      newState.card.text = ev.currentTarget.value;
-      this.setState(newState);
+    let card: Card | undefined = this.state.card;
+    if (card) {
+      card.text = ev.currentTarget.value;
+      this.setState(Object.assign({}, this.state, { card: card }));
     }
+  }
+
+  private setImage(image: string) {
+    let card: Card | undefined = this.state.card;
+    if (card) {
+      card.image = image;
+      this.setState(Object.assign({}, this.state, { card: card }));
+    }
+    this.closeImageSelector();
+  }
+
+  private closeImageSelector() {
+    this.setState(Object.assign({}, this.state, { imageSelectorOpen: false }));
+  }
+
+  private openImageSelector() {
+    this.setState(Object.assign({}, this.state, { imageSelectorOpen: true }));
   }
 }
