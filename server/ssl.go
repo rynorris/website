b941c294b914d71c24a071770fd731f9653090cd
Ryan Norris <rnorris@palantir.com>
2017-10-18 13:53:05 +0800

    Enable go 1.8 cipher suites

--- server/ssl.go
+++ server/ssl.go
@@ -19,14 +19,14 @@ func getTlsConfig(useAcme bool, cacheDir string, domains []string) *tls.Config {
 		// Only use curves which have assembly implementations
 		CurvePreferences: []tls.CurveID{
 			tls.CurveP256,
-			// tls.X25519, // Go 1.8 only
+			tls.X25519,
 		},
 		MinVersion: tls.VersionTLS12,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			// tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
-			// tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 
