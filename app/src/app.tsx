0b9bd76b27c916eedbfadb848d7f20aa6acc01e7
Ryan Norris <rnorris@palantir.com>
2019-01-03 18:43:34 +0000

    react-router 4

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,9 +1,9 @@
 import "es6-shim";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Redirect, Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import {createHistory} from "history";
-import {Redirect, Route, Router, useRouterHistory} from "react-router";
 import {Container} from "./container";
 import AboutPage from "./pages/about-page";
 import ContactPage from "./pages/contact-page";
@@ -11,23 +11,23 @@ import FrontPage from "./pages/front-page";
 import ServicesPage from "./pages/services-page";
 import TeachersPage from "./pages/teachers-page";
 
-const history = useRouterHistory(createHistory)({basename: "/"});
-
 const appElement = document.getElementById("app");
 
 if (appElement != null) {
   ReactDOM.render((
     <MuiThemeProvider>
-      <Router history={history}>
-        <Redirect from="/" to="/front" />
-        <Route path="/" component={Container}>
-          <Route path="front" component={FrontPage} />
-          <Route path="about" component={AboutPage} />
-          <Route path="teachers" component={TeachersPage} />
-          <Route path="services" component={ServicesPage} />
-          <Route path="contact" component={ContactPage} />
-        </Route>
-      </Router>
+      <BrowserRouter>
+        <Container>
+          <Switch>
+            <Route path="/front" component={FrontPage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/teachers" component={TeachersPage} />
+            <Route path="/services" component={ServicesPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Redirect from="*" to="/front" />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </MuiThemeProvider>
   ), appElement);
 } else {
