f326ebf4f27fe39b61706539c4f555b2172bfb0e
Ryan Norris <rnorris@palantir.com>
2019-01-03 20:56:15 +0000

    Simplify reducer code

--- app/src/state/reducer.ts
+++ app/src/state/reducer.ts
@@ -17,35 +17,16 @@ const initialState: AppState = {
 export const appReducer: Reducer<AppState> = (state = initialState, action: Action) => {
   switch (action.type) {
     case "LOGIN":
-      return Object.assign({}, state, {
-        auth: {
-          user: (action as LoginAction).user,
-        }
-      });
+      return { ...state, auth: { user: (action as LoginAction).user } };
 
     case "LOGOUT":
-      return Object.assign({}, state, {
-        auth: {
-          user: null,
-        }
-      });
+      return { ...state, auth: { user: null } };
 
     case "TOAST":
-      let toast: ToastAction = action as ToastAction;
-      return Object.assign({}, state, {
-        toaster: {
-          open: true,
-          text: toast.text,
-        }
-      });
+      return { ...state, toaster: { open: true, text: (action as ToastAction).text } };
 
     case "CLOSE-TOAST":
-      return Object.assign({}, state, {
-        toaster: {
-          open: false,
-          text: "",
-        }
-      });
+      return { ...state, toaster: { open: false, text: "" } };
 
     default:
       return state;
